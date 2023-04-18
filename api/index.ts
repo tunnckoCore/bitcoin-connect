035d746cebd3dcd8d3ac75124c5fef7556e2aaed
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2023-04-18 20:10:21 +0300

    chore(api): tweaks
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- api/index.ts
+++ api/index.ts
@@ -67,11 +67,7 @@ serve(async (req) => {
     { data },
     {
       status: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
+      headers: corsHeaders,
     }
   );
 });
